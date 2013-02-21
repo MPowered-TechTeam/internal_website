11d3595063f029af003653f20307ed3accecd2fb
Jeff Sallans <jeffsallans@gmail.com>
2013-02-20 20:31:31 -0500

    table from text

--- memberPage.js
+++ memberPage.js
@@ -2,6 +2,7 @@
 function getDataAndParse(idName) {
 	var data = document.getElementById(idName).innerHTML;
 	data = data.split("\n");
+	console.log(data);
 
 	for (var i = 0; i < data.length; i++) {
 		data[i] = data[i].split(",");
