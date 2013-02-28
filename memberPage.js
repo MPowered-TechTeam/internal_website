b29f555ee88a557545e36bb5a340590ffa324ffe
Harsha Gotur <hgotur@umich.edu>
2013-02-27 20:19:49 -0500

    zero indexed function

--- memberPage.js
+++ memberPage.js
@@ -1,12 +1,15 @@
 
 function getDataAndParse(idName) {
 	var data = document.getElementById(idName).innerHTML;
-	data = data.split("\n");
+	data = data.split(";");
 	console.log(data);
 
 	for (var i = 0; i < data.length; i++) {
 		data[i] = data[i].split(",");
+		console.log("i = ", i);
+		for (var j = 0; j < data[i].length; j++) {
+			console.log(data[i][j]);
+		}
 	}
-
 	return data;
 }
