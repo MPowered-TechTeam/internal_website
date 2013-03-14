e9f13402523700b3340dfdd2392e93582cfbd03d
Harsha Gotur <hgotur@umich.edu>
2013-03-13 20:14:35 -0400

    Fixed Table and Sort

--- buildtable.js
+++ buildtable.js
@@ -2,13 +2,26 @@ function build_table(table_data)
 {
 	var the_table = document.createElement('table');
 
+	the_table.className = "table";
 	
 	// Note, don't forget the var keyword!
-	var tr, td;
-	tr = document.createElement('th');
-	td = document.createElement('td');
-	td.appendChild(document.createTextNode("Name"));
-	tr.appendChild(td);
+	var tr, td, th;
+	tr = document.createElement('tr');
+	th = document.createElement('th');
+	th.appendChild(document.createTextNode("Name"));
+	tr.appendChild(th);
+	th = document.createElement('th');
+	th.appendChild(document.createTextNode("Uniqname"));
+	tr.appendChild(th);
+	th = document.createElement('th');
+	th.appendChild(document.createTextNode("Team"));
+	tr.appendChild(th);
+	th = document.createElement('th');
+	th.appendChild(document.createTextNode("Status"));
+	tr.appendChild(th);
+	th = document.createElement('th');
+	th.appendChild(document.createTextNode("Class"));
+	tr.appendChild(th);
     the_table.appendChild(tr);
 
 	for (var j = 0; j < parseInt(table_data.length); j++)
@@ -23,8 +36,19 @@ function build_table(table_data)
 	}
 	
 	document.getElementById('member_table').appendChild(the_table);
+
 	$('#member_table').sortTable({
+          onCol: 4,
+          keepRelationships: true
+    });
+
+    $('#member_table').sortTable({
           onCol: 1,
           keepRelationships: true
     });
 }
+
+/*
+$(function() {
+    $("table#member_table").tablesorter({ sortList: [[1,0]] });
+  });*/
--- memberPage.js
+++ memberPage.js
@@ -5,11 +5,8 @@ function getDataAndParse(idName) {
 	console.log(data);
 
 	for (var i = 0; i < data.length; i++) {
+		data[i] = data[i].replace(/\s+/,'');
 		data[i] = data[i].split(",");
-		console.log("i = ", i);
-		for (var j = 0; j < data[i].length; j++) {
-			console.log(data[i][j]);
-		}
 	}
 	return data;
 }
